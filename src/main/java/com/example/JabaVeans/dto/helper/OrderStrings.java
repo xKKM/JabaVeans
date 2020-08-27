25c439d07eb08ca493bede9e8bf640bc523f6ff4
xKKM <41371383+xKKM@users.noreply.github.com>
2020-08-27 20:07:32 +0200

    Add helper object OrderStrings

---
+++ src/main/java/com/example/JabaVeans/dto/helper/OrderStrings.java
@@ -0,0 +1,13 @@
+package com.example.JabaVeans.dto.helper;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
+public class OrderStrings {
+    public static final String[] orderStates = {"Nowe", "Realizowane","Wysłane","Dostarczone"};
+    public static final String[] orderWeights = {"100g", "250g","500g","1000g"};
+
+}
