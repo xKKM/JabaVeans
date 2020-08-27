a8872c86dcc528963264bdb5e93e923674c40157
xKKM <41371383+xKKM@users.noreply.github.com>
2020-08-27 20:25:34 +0200

    Add IndexController tests

---
+++ src/test/java/com/example/JabaVeans/controller/IndexControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.JabaVeans.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class IndexControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void indexRenders() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("index"));
+    }
+
+}
