92a7ab42ec87c17d9a37da663c1ba2684e764fb9
xKKM <41371383+xKKM@users.noreply.github.com>
2020-08-27 20:09:38 +0200

    Add EmailService interface implementation EmailServiceImpl

---
+++ src/main/java/com/example/JabaVeans/service/EmailServiceImpl.java
@@ -0,0 +1,67 @@
+package com.example.JabaVeans.service;
+
+import com.example.JabaVeans.dto.Order;
+import com.example.JabaVeans.dto.helper.OrderStrings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+    private final JavaMailSender emailSender;
+
+    String from;
+
+    @Autowired
+    public EmailServiceImpl(JavaMailSender emailSender, @Value("${spring.mail.username}") String from) {
+        this.emailSender = emailSender;
+        this.from = from;
+    }
+
+
+
+    @Override
+    @Async
+    public void sendOrderConfirmation(Order order) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(order.getEmail());
+        message.setSubject("Java Beans potwierdzenie.");
+
+        String text = "Przyjeliśmy zamówienie dla %s %s. \n\n " +
+                "Sprawdź szczegóły oraz stan zamówienia na naszej stronie wpisjąc numer zamówienia %d.";
+
+        message.setText(
+                String.format(
+                        text,
+                        order.getFirstName(),
+                        order.getSurname(),
+                        order.getOrderID()));
+        emailSender.send(message);
+    }
+
+    @Override
+    @Async
+    public void sendOrderStateChange(Order order) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(order.getEmail());
+        message.setSubject("Java Beans zmiana stanu zamówienia.");
+
+        String text = "Zamówienie dla %s %s zmieniło stan na %s.\n\n " +
+                "Sprawdź szczegóły oraz stan zamówienia na naszej stronie wpisjąc numer zamówienia %d.";
+
+        message.setText(
+                String.format(
+                        text,
+                        order.getFirstName(),
+                        order.getSurname(),
+                        OrderStrings.orderStates[order.getOrderState()],
+                        order.getOrderID()));
+
+        emailSender.send(message);
+    }
+}
